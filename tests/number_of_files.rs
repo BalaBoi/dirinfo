d29adfcf6967e0e8d71b05b51cb03091494636fe
BalaBoi <sanjay.krishnan456@gmail.com>
2025-01-21 01:53:15 +0530

    dir_exists integration test

--- tests/number_of_files.rs
+++ tests/number_of_files.rs
@@ -12,3 +12,20 @@ fn dir_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn dir_exists_with_files() -> Result<(), Box<dyn std::error::Error>> {
+    let tmpdir = assert_fs::TempDir::new()?;
+    let file1 = std::fs::File::create(tmpdir.path().join("temp1"))?;
+    file1.set_len(1234)?;
+    let file2 = std::fs::File::create(tmpdir.path().join("temp2"))?;
+    file2.set_len(1234)?;
+
+    let mut cmd = Command::cargo_bin("dirinfo")?;
+    cmd.arg(tmpdir.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("Logical size of all the files: 2468"));
+
+    Ok(())
+}
