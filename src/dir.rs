af1d606ff9fdab29b61ded9c4d13973498de8040
BalaBoi <sanjay.krishnan456@gmail.com>
2025-01-21 20:41:33 +0530

    unique entries

--- src/dir.rs
+++ src/dir.rs
@@ -1,7 +1,6 @@
 use anyhow::{anyhow, Result};
 use std::{
-    fs::read_dir,
-    path::{Path, PathBuf},
+    collections::HashSet, fs::read_dir, os::linux::fs::MetadataExt, path::{Path, PathBuf}
 };
 
 use crate::info::Info;
@@ -22,25 +21,41 @@ impl Dir {
     }
 
     pub fn info(&self) -> Result<Info> {
-        let (size, n_files) = n_files(&self.path)?;
+        let (size, n_files) = n_files(&self.path, &mut HashSet::new())?;
         Ok(Info::new(size, n_files))
     }
 }
 
-fn n_files(path: &Path) -> Result<(u64, usize)> {
+fn n_files(path: &Path, set: &mut HashSet<FileEntry>) -> Result<(u64, usize)> {
     let mut n = 0;
-    let mut size = 0;
+    let mut size = path.metadata()?.st_blocks() * 512;
     for entry in read_dir(path)? {
         let entry = entry?;
         let path = entry.path();
+        let metadata = entry.metadata()?;
+        let f_entry = FileEntry {
+            inode: metadata.st_ino(),
+            device: metadata.st_dev(),
+        };
+        if set.contains(&f_entry) {
+            continue;
+        } else {
+            set.insert(f_entry);
+        }
         if path.is_file() {
             n += 1;
-            size += path.metadata()?.len();
+            size += metadata.st_blocks() * 512;
         } else if path.is_dir() {
-            let (s, m) = n_files(&path)?;
+            let (s, m) = n_files(&path, set)?;
             n += m;
-            size += s;
+            size += s + metadata.st_blocks() * 512;
         }
     }
     Ok((size, n))
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct FileEntry {
+    inode: u64,
+    device: u64,
 }
--- src/info.rs
+++ src/info.rs
@@ -12,7 +12,7 @@ impl Info {
     pub fn display(&self, mut writer: impl std::io::Write) -> Result<()> {
         writeln!(
             writer,
-            "Number of files: {}\nLogical size of all the files: {}\n",
+            "Number of files: {}\nLogical size of all the files: {}",
             self.n_files,
             file_size::fit_4(self.size)
         )
